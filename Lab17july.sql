32f000e32e7e606cc3cb03f2b816c5ea778bd5e7
Jiraporn Laemee <jiraporn30797@gmail.com>
2025-07-17 15:53:51 +0700

    Add files via upload

---
+++ Lab17july.sql
@@ -0,0 +1,6 @@
+SELECT * from Products;
+SELECT * from Employees;
+SELECT EmployeeID, FirstName, LastName from Employees;
+SELECT * from Employees Where City = 'London';
+SELECT * from Categories;
+SELECT * from Customers; 
