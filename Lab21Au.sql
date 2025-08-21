c028930af040b834e2148a0f1bace080763daa7c
Jiraporn Laemee <jiraporn30797@gmail.com>
2025-08-21 13:53:54 +0700

    Update Lab21Au.sql

---
+++ Lab21Au.sql
@@ -0,0 +1,13 @@
+-- แสดงชื่อสินค้า ชื่อสินค้า และ ราคาสินค้า
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products, Categories
+Where Products.CategoryID = Categories.CategoryID
+and CategoryName = 'seafood'
+
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products AS p JOIN Categories as C
+ON p.CategoryID = C.CategoryID
+WHERE CategoryName = 'seafood'
+
+
+
