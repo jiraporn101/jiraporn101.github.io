da8c884ad1960562e62afeb122a54ae7450d3aba
Jiraporn Laemee <jiraporn30797@gmail.com>
2025-09-25 14:26:12 +0700

    Add files via upload

--- Lap25sep.sql
+++ Lap25sep.sql
@@ -1,6 +1,31 @@
----- สร้างชื่อฐานข้อมูล Zoo
+-- สร้างฐานข้อมูล Zoo
+CREATE DATABASE Zoo;
+GO
 
-CREATE DATABase Zoo
+-- แสดงรายการฐานข้อมูล
+SELECT name FROM sys.databases;
+GO
 
-SELECT * from sysdatabases
+-- ปรับ Collation ให้รองรับภาษาไทย
+ALTER DATABASE Zoo COLLATE Thai_CI_AS;
+GO
 
+-- ใช้งานฐานข้อมูล Zoo
+USE Zoo;
+GO
+
+-- สร้างตาราง AnimalType
+CREATE TABLE AnimalType (
+    TypeID NCHAR(3) NOT NULL PRIMARY KEY,
+    TName NVARCHAR(50) NOT NULL
+);
+GO
+
+-- เพิ่มข้อมูล
+INSERT INTO AnimalType VALUES ('T01', N'สัตว์ปีก');
+INSERT INTO AnimalType VALUES ('T02', N'สัตว์น้ำ');
+GO
+
+-- แสดงข้อมูลทั้งหมด
+SELECT * FROM AnimalType;
+GO
