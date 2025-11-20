27c99cc67cc62241d9b1c1a3be88ca49b39c2ad6
Jiraporn Laemee <jiraporn30797@gmail.com>
2025-11-20 10:54:16 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "Hello, World";
+        echo "<br>";
+        echo "<h2>My name is Jirapron.</h2>";
+        echo "<br>";
+        echo "<h3>PHP version: " . phpversion() . "</h3>";
+        phpinfo();
+
+    ?>
+</body>
+</html>
